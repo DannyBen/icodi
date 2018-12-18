75b79251517b9c9b191dbbf5add9579806431baf
Danny Ben Shitrit <db@dannyben.com>
2018-12-18 18:57:14 +0200

    test coverage 100%

--- spec/icodi/icodi_spec.rb
+++ spec/icodi/icodi_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Icodi do
   context "given a string" do
-    subject { described_class.new "thanks victor" }
+    subject { described_class.new "thanks victor", jitter: 0.3 }
     
     it "generates consistent svg" do
       expect(subject.to_s).to match_fixture 'render1'
